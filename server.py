c0c2c5bc9318ff199345ead42d41b6dfedde4331
VladimirTsanov <vtsanov2005@gmail.com>
2025-09-05 21:06:51 +0300

    Basic HTML files are added

--- server.py
+++ server.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/login')
 def index():
-    return 'Index Pageeeee'
+    return render_template("login.html")
 
-@app.route('/hello')
+@app.route('/register')
 def hello():
-    return 'Hello, World'
+    return render_template("registration.html")
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    return f'Subpath {escape(subpath)}'
+@app.route('/new-post')
+def add_post():
+    return render_template("add_post.html")
+
+@app.route('/edit-post')
+def edit_post():
+    return render_template("edit_post.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
